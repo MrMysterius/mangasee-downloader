8463ad4d601cc3b7b02bdf3aca167e252c27170c
MrMysterius <official.mrmysterius@gmail.com>
2023-01-28 04:14:33 +0100

    feat: added command selection switch

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 import { parse } from "https://deno.land/std@0.174.0/flags/mod.ts";
+import * as Color from "https://deno.land/std@0.174.0/fmt/colors.ts";
 
 const ARGS = parse(Deno.args);
+
+switch (ARGS._[0] || undefined) {
+  default:
+    console.log(Color.red("# No Command Supplied - Aborting #"));
+}

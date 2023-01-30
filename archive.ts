ac0068d29a8602c26d77fef1bb254dfa46536071
MrMysterius <official.mrmysterius@gmail.com>
2023-01-30 09:35:20 +0100

    fix: archiving function fix for download function

--- archive.ts
+++ archive.ts
@@ -1,7 +1,10 @@
 import { normalize, posix } from "https://deno.land/std@0.174.0/path/mod.ts";
 
 export async function archive(src_folder: string, archive_name: string, delete_src = true): Promise<boolean> {
-  const cmd: string[] = ["7z", "a", archive_name, posix.normalize(`.\\${posix.normalize(normalize(`${src_folder}`))}\\*`)];
+  try {
+    Deno.removeSync(archive_name, { recursive: true });
+  } catch (_e) {}
+  const cmd: string[] = ["7z", "a", archive_name, posix.normalize(`${posix.normalize(normalize(`${src_folder}`))}\\*`)];
 
   const process = Deno.run({
     cmd,

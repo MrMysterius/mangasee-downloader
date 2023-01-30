2f884d9c2234843edda033e83744b7e13cad8f9d
MrMysterius <official.mrmysterius@gmail.com>
2023-01-30 09:35:54 +0100

    build: updated building files

--- build_linux.sh
+++ build_linux.sh
@@ -1,2 +1,2 @@
 # Linux
-deno compile --allow-net --allow-read --allow-write --allow-env --target x86_64-unknown-linux-gnu --output mangasee-dl main.ts
+deno compile --allow-net --allow-read --allow-write --allow-env --allow-run --target x86_64-unknown-linux-gnu --output mangasee-dl main.ts
--- build_windows.sh
+++ build_windows.sh
@@ -1,2 +1,2 @@
 # Windows
-deno compile --allow-net --allow-read --allow-write --allow-env --target x86_64-pc-windows-msvc --output mangasee-dl.exe main.ts
+deno compile --allow-net --allow-read --allow-write --allow-env --allow-run --target x86_64-pc-windows-msvc --output mangasee-dl.exe main.ts

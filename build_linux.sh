a7b66204d70e3a49776adafa9c26a008d97a2c35
Mr Mysterius <25854814+MrMysterius@users.noreply.github.com>
2024-08-28 16:42:07 +0200

    remove: unused files

--- build_linux.sh
+++
@@ -1,2 +0,0 @@
-# Linux
-deno compile --allow-net --allow-read --allow-write --allow-env --allow-run --target x86_64-unknown-linux-gnu --output mangasee-dl main.ts
--- build_windows.sh
+++
@@ -1,2 +0,0 @@
-# Windows
-deno compile --allow-net --allow-read --allow-write --allow-env --allow-run --target x86_64-pc-windows-msvc --output mangasee-dl.exe main.ts
